ae590b382bddfbb19918f61573fb0b3e17b73a7f
Noelle Brown <noelle.brown@generalassemb.ly>
2020-03-14 16:09:13 -0600

    add tuesday materials

---
+++ Week_01/01.2_Tuesday/Warm-Up/is_integer.py
@@ -0,0 +1,31 @@
+'''
+Define a function is_int() that takes a number, x, as an input.
+Have it return True if the number is an integer (as defined below) and False otherwise.
+
+An integer is just a whole number, meaning a number without a decimal.
+For instance, -17, 0, and 42 are all integers, but 98.6 is not.
+
+>>> is_int(0)
+True
+
+>>> is_int(1)
+True
+
+>>> is_int(100)
+True
+
+>>> is_int(95.7)
+False
+
+'''
+
+def is_int(x):
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+is_int(27)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
