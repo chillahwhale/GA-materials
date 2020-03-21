e5d5a6667fdf03e795679443e92c96c806e84ca3
Noelle Brown <noelle.brown@generalassemb.ly>
2020-03-21 17:55:44 -0600

    add thurs warm up

---
+++ Week_02/02.4_Thursday/Warm-Up/product.py
@@ -0,0 +1,22 @@
+'''
+Define a function called product() that takes a list of integers as input 
+and returns the product of all of the elements in the list as an integer.
+Assume the list has at least one element in it.
+
+>>> product([5])
+5
+
+>>> product([4, 5, 5])
+100
+
+'''
+
+def product(integers):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
