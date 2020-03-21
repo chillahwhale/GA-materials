c40fc7afb79fd8fb6fd62369c530f34119707de2
Noelle Brown <noelle.brown@generalassemb.ly>
2020-03-21 17:55:04 -0600

    add wed warm up

---
+++ Week_02/02.3_Wednesday/Warm-Up/count.py
@@ -0,0 +1,23 @@
+'''
+Define a function called count() that has two arguments, called sequence (a list) and item.
+The item you input may be an integer, string or float.
+Return an integer representing the number of times the item occurs in the sequence.
+
+>>> count([1, 4, 1, 3, 1, 6], 1)
+3
+
+>>> count(["boat", "kayak", 1, 3, "boat"], "boat")
+2
+'''
+def count(sequence, item):
+    
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+count(["hello", "poodle", "chocolate"], "chocolate")
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
