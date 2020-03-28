d3cd47679116baf0cccd7595df87a8b0eea9e8a2
Noelle Brown <noelle.brown@generalassemb.ly>
2020-03-28 13:02:44 -0600

    add thurs warm up

---
+++ Week_03/03.4_Thursday/Warm-Up/reverse_list.py
@@ -0,0 +1,21 @@
+''' 
+Write a function that takes in a list and returns a new, reversed version of the list.
+Do not use the list object's .reverse() method.
+
+>>> reverse_array([1, 2, 3, 4, 5])
+[5, 4, 3, 2, 1]
+
+>>> reverse_array(['apple', 'boat', 'cat', 'dog'])
+['dog', 'cat', 'boat', 'apple']
+
+'''
+
+def reverse_array(my_list):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
