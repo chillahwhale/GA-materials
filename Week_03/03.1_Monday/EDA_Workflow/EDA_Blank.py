ff8fb894339ec5ef4c02c6018e4ef4003835cd66
Noelle Brown <noelle.brown@generalassemb.ly>
2020-03-28 12:55:39 -0600

    adding monday materials

---
+++ Week_03/03.1_Monday/EDA_Workflow/EDA_Blank.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# clean the data prior to analysis
+class CleanData:
+
+    def __init__(self, data):
+        self.data = data
+
+    def a(self):
+        pass
+
+    def b(self):
+        pass
+
+    def c(self):
+        pass
---
+++ Week_03/03.1_Monday/Warm-Up/is_anagram.py
@@ -0,0 +1,24 @@
+'''Write a function called is_anagram() that takes in 2 strings as arguments.
+Return True if the strings are anagrams of each other (ie: they contain the same
+letters and no extra letters). Otherwise, return false.
+
+>>> is_anagram("cat", "dog")
+False
+
+>>> is_anagram("care", "race")
+True
+
+'''
+
+def is_anagram(text1, text2):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+is_anagram("blue", "black")
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
