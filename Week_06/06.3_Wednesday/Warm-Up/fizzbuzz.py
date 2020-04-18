990a67282c37a60811849328e8c01a5a79d00658
Noelle Brown <noelle.brown@generalassemb.ly>
2020-04-18 15:28:34 -0600

    add wed warm up

---
+++ Week_06/06.3_Wednesday/Warm-Up/fizzbuzz.py
@@ -0,0 +1,26 @@
+''' Write a funciton called fizzbuzz() which takes a number (n) as an argument and returns a list. 
+For each integer between 1 and n, if the integer is divisible by 3, append "Fizz" 
+to the result list. If the integer is divisible by 5, append "Buzz" to the result list. 
+If the integer is divisible by both 3 and 5, append "FizzBuzz" to the result list. If the integer
+is not evenly divisible by 3 or 5, append the integer itself to the result list.
+
+>>> fizzbuzz(15)
+[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+
+'''
+
+def fizzbuzz(n):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+fizzbuzz(100)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+
+    
