91a3bde994a4e133e36a3e3d6a76ae00ff064be8
Noelle Brown <noelle.brown@generalassemb.ly>
2020-04-24 15:27:39 -0600

    add thurs materials

---
+++ Week_07/07.4_Thursday/Warm-Up/common_chars.py
@@ -0,0 +1,25 @@
+'''
+Write a function called common_chars() that takes in two lowercase strings,
+text1 and text2, as arguments. Return a list of the common characters
+contained in the two strings.
+
+>>> common_chars("donuts", "tundra")
+['d', 'n', 'u', 't']
+
+>>> common_chars("peaches", "door")
+[]
+
+'''
+
+def common_chars(text1, text2):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+common_chars("stem", "pick")
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
