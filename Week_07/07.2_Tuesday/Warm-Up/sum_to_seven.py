38fd3b2bdfdf820db30bfca97e077c78532ff3d4
Noelle Brown <noelle.brown@generalassemb.ly>
2020-04-24 15:26:39 -0600

    add tuesday materials

---
+++ Week_07/07.2_Tuesday/Warm-Up/sum_to_seven.py
@@ -0,0 +1,31 @@
+'''
+Write a function  called lucky_sevens(nums) which takes in a list of integers 
+and returns true if any three consecutive elements sum to 7.
+
+Test cases:
+
+>>> lucky_sevens([2,1,5,1,0])
+True
+
+>>> lucky_sevens([0,-2,1,8])
+True
+
+>>> lucky_sevens([7,7,7,7])
+False
+
+>>> lucky_sevens([3,4,3,4])
+False
+'''
+
+def lucky_sevens(nums):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+lucky_sevens([5, 3, 2, 2])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
