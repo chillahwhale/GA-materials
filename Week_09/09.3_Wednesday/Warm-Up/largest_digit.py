c721f206a98470a7bb7a20acc95e771c64c903e4
Noelle Brown <noelle.brown@generalassemb.ly>
2020-05-08 15:38:51 -0600

    add wed warm up

---
+++ Week_09/09.3_Wednesday/Warm-Up/largest_digit.py
@@ -0,0 +1,32 @@
+'''
+Problem from Codewars (https://www.codewars.com/kata/51675d17e0c1bed195000001)
+
+In the following 10 digit number:
+    1234567890
+67890 is the greatest sequence of 5 consecutive digits.
+
+Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer.
+
+>>> largest_digit('1234567890')
+67890
+
+>>> largest_digit('5729471048572648292465023')
+94710
+
+>>> largest_digit('98361857628')
+98361
+
+'''
+
+def largest_digit(n):
+
+    # Replace the line below with all your code.
+    pass
+
+
+largest_digit('1234567890')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
